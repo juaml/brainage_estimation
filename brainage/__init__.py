68e4bbdb8ce39517642caf81ea22292c297f0e4e
Shammi More <s.more@fz-juelich.de>
2022-10-18 15:32:09 +0200

    add more files to brainage package

--- brainage/__init__.py
+++ brainage/__init__.py
@@ -1,2 +1,6 @@
 from .read_data_mask_resampled import binarize_3d
 from .read_data_mask_resampled import read_sub_data
+from .create_splits import stratified_splits
+from .xgboost_adapted import XGBoostAdapted
+from .zscore import ZScoreSubwise, ZScore
+from .create_splits import repeated_stratified_splits
