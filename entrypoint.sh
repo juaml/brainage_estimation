5527baebdd4548501fd91e3ee3ae324a201ccf6b
Shammi More <s.more@fz-juelich.de>
2024-08-16 15:56:51 +0200

    entrypoint which runs with container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cd codes
-python3 predict_age.py --features_path $1 --subject_filepaths $2 --output_path $3 --output_prefix $4 --mask_file ../masks/brainmask_12.8.nii --smooth_fwhm $5  --resample_size $6 --model_file $7
-
-
-
+python3 predict_age_sing.py --features_path $1 --data_dir $2 --subject_filepaths $3 --output_path $4 --output_prefix $5 --mask_file ../masks/brainmask_12.8.nii --smooth_fwhm $6  --resample_size $7 --model_file $8
